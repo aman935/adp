06f786ae7fd363bc2274f49bc6255d6e253e38a0
dhairya0904 <dhairya60@gmail.com>
2016-11-11 02:16:51 +0530

    more_button_aded

--- Sample_App/config/routes.rb
+++ Sample_App/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
+  
   get  '/about',  to: 'static_pages#about'
   get  '/help',   to: 'static_pages#help' 
   get  '/signup',  to: 'users#new'
