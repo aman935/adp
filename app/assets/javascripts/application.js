d7abb58c1196f67ddd42801a7c93f0a595acbb37
Aman <singh.amansingh935@gmail.com>
2016-11-14 12:41:03 +0530

    Changed the directory structure

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,38 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+
+
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require turbolinks
+//= require_tree 
+//= require jquery.turbolinks
+
+
+      function updateSource() { 
+
+        var audio = document.getElementById('audio-player');
+        var source = document.getElementById('oggSource');
+
+        source.src =  "/Blue Browne.ogg";
+
+       
+        audio.load(); //call this to just preload the audio without playing
+        audio.play(); //call this to play the song right away
+    }
---
+++ db/migrate/20161111205641_create_audios.rb
@@ -0,0 +1,12 @@
+class CreateAudios < ActiveRecord::Migration[5.0]
+  def change
+    create_table :audios do |t|
+      t.string :name
+      t.string :genre
+      t.string :path
+      t.string :artist
+
+      t.timestamps
+    end
+  end
+end
